5da221374ff92e09e1093c3f5ec1afe1d492984c
Jason Volk <jason@zemos.net>
2024-04-04 23:41:46 -0700

    client-api: Add cache-control header to media responses.
    Signed-off-by: Jason Volk <jason@zemos.net>

--- crates/ruma-client-api/src/media/get_content.rs
+++ crates/ruma-client-api/src/media/get_content.rs
@@ -9,7 +9,7 @@ pub mod v3 {
 
     use std::time::Duration;
 
-    use http::header::{CONTENT_DISPOSITION, CONTENT_TYPE};
+    use http::header::{CACHE_CONTROL, CONTENT_DISPOSITION, CONTENT_TYPE};
     use ruma_common::{
         IdParseError, MxcUri, OwnedServerName,
         api::{auth_scheme::NoAccessToken, request, response},
@@ -97,6 +97,14 @@ pub mod v3 {
         /// [MDN]: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cross-Origin-Resource-Policy#syntax
         #[ruma_api(header = CROSS_ORIGIN_RESOURCE_POLICY)]
         pub cross_origin_resource_policy: Option<String>,
+
+        /// The value of the `Cache-Control` HTTP header.
+        ///
+        /// See [MDN] for the syntax.
+        ///
+        /// [MDN]: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cache-Control#syntax
+        #[ruma_api(header = CACHE_CONTROL)]
+        pub cache_control: Option<String>,
     }
 
     #[allow(deprecated)]
@@ -134,6 +142,7 @@ pub mod v3 {
                 content_type: Some(content_type),
                 content_disposition: Some(content_disposition),
                 cross_origin_resource_policy: Some("cross-origin".to_owned()),
+                cache_control: None,
             }
         }
     }
