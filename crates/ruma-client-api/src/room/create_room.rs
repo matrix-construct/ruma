23be7cbd3af68492ac6e0ba1b48ee377f6a69fba
strawberry <strawberry@puppygock.gay>
2024-05-01 15:27:48 -0400

    add custom room ID field to room creation request body
    Signed-off-by: strawberry <strawberry@puppygock.gay>

--- crates/ruma-client-api/src/room/create_room.rs
+++ crates/ruma-client-api/src/room/create_room.rs
@@ -90,6 +90,10 @@ pub mod v3 {
         #[serde(skip_serializing_if = "Option::is_none")]
         pub room_alias_name: Option<String>,
 
+        /// The desired custom room ID local part.
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub room_id: Option<String>,
+
         /// Room version to set for the room.
         ///
         /// Defaults to homeserver's default if not specified.
