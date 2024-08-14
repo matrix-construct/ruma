b7fde6088bd51d4471ab36b40e1946a351f16a92
Jason Volk <jason@zemos.net>
2024-08-14 02:09:52 -0400

    add missing corp/cache-control headers to client authenticated media
    Signed-off-by: Jason Volk <jason@zemos.net>
    Signed-off-by: strawberry <strawberry@puppygock.gay>

--- crates/ruma-client-api/src/authenticated_media/get_content_as_filename.rs
+++ crates/ruma-client-api/src/authenticated_media/get_content_as_filename.rs
@@ -9,12 +9,14 @@ pub mod v1 {
 
     use std::time::Duration;
 
-    use http::header::{CONTENT_DISPOSITION, CONTENT_TYPE};
+    use http::header::{CACHE_CONTROL, CONTENT_DISPOSITION, CONTENT_TYPE};
     use ruma_common::{
         api::{request, response, Metadata},
         http_headers::ContentDisposition,
         metadata, IdParseError, MxcUri, OwnedServerName,
     };
+
+    use crate::http_headers::CROSS_ORIGIN_RESOURCE_POLICY;
 
     const METADATA: Metadata = metadata! {
         method: GET,
@@ -69,6 +71,26 @@ pub mod v1 {
         /// file that was previously uploaded.
         #[ruma_api(header = CONTENT_DISPOSITION)]
         pub content_disposition: Option<ContentDisposition>,
+
+        /// The value of the `Cross-Origin-Resource-Policy` HTTP header.
+        ///
+        /// See [MDN] for the syntax.
+        ///
+        /// [MDN]: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cross-Origin-Resource-Policy#syntax
+        ///
+        /// TODO: make this use Cow static str's
+        #[ruma_api(header = CROSS_ORIGIN_RESOURCE_POLICY)]
+        pub cross_origin_resource_policy: Option<String>,
+
+        /// The value of the `Cache-Control` HTTP header.
+        ///
+        /// See [MDN] for the syntax.
+        ///
+        /// [MDN]: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cache-Control#syntax
+        ///
+        /// TODO: make this use Cow static str's
+        #[ruma_api(header = CACHE_CONTROL)]
+        pub cache_control: Option<String>,
     }
 
     impl Request {
@@ -101,6 +123,8 @@ pub mod v1 {
                 file,
                 content_type: Some(content_type),
                 content_disposition: Some(content_disposition),
+                cross_origin_resource_policy: None,
+                cache_control: None,
             }
         }
     }
