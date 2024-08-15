0e53e10f2801e70ddd63f62e4492917e4f15f58f
Jason Volk <jason@zemos.net>
2024-08-15 23:38:44 +0000

    structured Mxc type
    fix str to mxc conversion; add convenience OwnedMxcUri conversion
    
    fix tests for structured Mxc
    
    Signed-off-by: Jason Volk <jason@zemos.net>

--- crates/ruma-common/src/identifiers/mxc_uri.rs
+++ crates/ruma-common/src/identifiers/mxc_uri.rs
@@ -2,7 +2,7 @@
 //!
 //! [MXC URI]: https://spec.matrix.org/latest/client-server-api/#matrix-content-mxc-uris
 
-use std::num::NonZeroU8;
+use std::{fmt, num::NonZeroU8};
 
 use ruma_identifiers_validation::{error::MxcUriError, mxc_uri::validate};
 use ruma_macros::IdDst;
@@ -18,25 +18,34 @@ type Result<T, E = MxcUriError> = std::result::Result<T, E>;
 #[derive(PartialEq, Eq, PartialOrd, Ord, Hash, IdDst)]
 pub struct MxcUri(str);
 
+/// Structured MXC URI which may reference strings from separate sources without serialization
+#[non_exhaustive]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Mxc<'a> {
+    /// ServerName part of the MXC URI
+    pub server_name: &'a ServerName,
+
+    /// MediaId part of the MXC URI
+    pub media_id: &'a str,
+}
+
 impl MxcUri {
     /// If this is a valid MXC URI, returns the media ID.
     pub fn media_id(&self) -> Result<&str> {
-        self.parts().map(|(_, s)| s)
+        self.parts().map(|mxc| mxc.media_id)
     }
 
     /// If this is a valid MXC URI, returns the server name.
     pub fn server_name(&self) -> Result<&ServerName> {
-        self.parts().map(|(s, _)| s)
+        self.parts().map(|mxc| mxc.server_name)
     }
 
     /// If this is a valid MXC URI, returns a `(server_name, media_id)` tuple, else it returns the
     /// error.
-    pub fn parts(&self) -> Result<(&ServerName, &str)> {
-        self.extract_slash_idx().map(|idx| {
-            (
-                ServerName::from_borrowed(&self.as_str()[6..idx.get() as usize]),
-                &self.as_str()[idx.get() as usize + 1..],
-            )
+    pub fn parts(&self) -> Result<Mxc<'_>> {
+        self.extract_slash_idx().map(|idx| Mxc::<'_> {
+            server_name: ServerName::from_borrowed(&self.as_str()[6..idx.get() as usize]),
+            media_id: &self.as_str()[idx.get() as usize + 1..],
         })
     }
 
@@ -58,11 +67,43 @@ impl MxcUri {
     }
 }
 
+impl fmt::Display for Mxc<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "mxc://{}/{}", self.server_name, self.media_id)
+    }
+}
+
+impl<'a> TryFrom<&'a MxcUri> for Mxc<'a> {
+    type Error = MxcUriError;
+
+    fn try_from(s: &'a MxcUri) -> Result<Self, Self::Error> {
+        s.parts()
+    }
+}
+
+impl<'a> TryFrom<&'a str> for Mxc<'a> {
+    type Error = MxcUriError;
+
+    fn try_from(s: &'a str) -> Result<Self, Self::Error> {
+        let s: &MxcUri = s.into();
+        s.try_into()
+    }
+}
+
+impl<'a> TryFrom<&'a OwnedMxcUri> for Mxc<'a> {
+    type Error = MxcUriError;
+
+    fn try_from(s: &'a OwnedMxcUri) -> Result<Self, Self::Error> {
+        let s: &MxcUri = s.as_ref();
+        s.try_into()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use ruma_identifiers_validation::error::MxcUriError;
 
-    use super::{MxcUri, OwnedMxcUri};
+    use super::{Mxc, MxcUri, OwnedMxcUri};
 
     #[test]
     fn parse_mxc_uri() {
@@ -71,7 +112,10 @@ mod tests {
         assert!(mxc.is_valid());
         assert_eq!(
             mxc.parts(),
-            Ok(("127.0.0.1".try_into().expect("Failed to create ServerName"), "asd32asdfasdsd"))
+            Ok(Mxc {
+                server_name: "127.0.0.1".try_into().expect("Failed to create ServerName"),
+                media_id: "asd32asdfasdsd",
+            })
         );
     }
 
@@ -106,7 +150,10 @@ mod tests {
         assert!(mxc.is_valid());
         assert_eq!(
             mxc.parts(),
-            Ok(("server".try_into().expect("Failed to create ServerName"), "1234id"))
+            Ok(Mxc {
+                server_name: "server".try_into().expect("Failed to create ServerName"),
+                media_id: "1234id"
+            })
         );
     }
 }
