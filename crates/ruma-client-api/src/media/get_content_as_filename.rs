ce4a590b2fa3947ba03801ba0ba1965fc001417e
Jason Volk <jason@zemos.net>
2024-08-15 23:38:44 +0000

    structured Mxc type
    fix str to mxc conversion; add convenience OwnedMxcUri conversion
    
    fix tests for structured Mxc
    
    Signed-off-by: Jason Volk <jason@zemos.net>

--- crates/ruma-client-api/src/media/get_content_as_filename.rs
+++ crates/ruma-client-api/src/media/get_content_as_filename.rs
@@ -11,7 +11,7 @@ pub mod v3 {
 
     use http::header::{CACHE_CONTROL, CONTENT_DISPOSITION, CONTENT_TYPE};
     use ruma_common::{
-        IdParseError, MxcUri, OwnedServerName,
+        IdParseError, Mxc, MxcUri, OwnedServerName,
         api::{auth_scheme::NoAccessToken, request, response},
         http_headers::{CROSS_ORIGIN_RESOURCE_POLICY, ContentDisposition},
         metadata,
@@ -127,7 +127,7 @@ pub mod v3 {
 
         /// Creates a new `Request` with the given url and filename.
         pub fn from_url(url: &MxcUri, filename: String) -> Result<Self, IdParseError> {
-            let (server_name, media_id) = url.parts()?;
+            let Mxc { server_name, media_id, .. } = url.parts()?;
 
             Ok(Self::new(media_id.to_owned(), server_name.to_owned(), filename))
         }
