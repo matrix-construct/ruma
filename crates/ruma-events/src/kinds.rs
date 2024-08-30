43f1ae3ff0d085f773b15afbe8eafb3f7a61e785
morguldir <morguldir@protonmail.com>
2024-08-30 09:10:52 +0200

    accountdata: add an enum to allow returning both global and room accountdata

--- crates/ruma-events/src/kinds.rs
+++ crates/ruma-events/src/kinds.rs
@@ -19,6 +19,27 @@ use super::{
 };
 #[cfg(feature = "unstable-msc4354")]
 use crate::sticky::StickyObject;
+use crate::{AnyGlobalAccountDataEvent, AnyRoomAccountDataEvent};
+
+/// Enum allowing to use the same structures for global and room account data
+#[derive(Debug)]
+#[allow(clippy::exhaustive_enums)]
+pub enum AnyAccountDataEvent {
+    /// An event for a specific room
+    Room(AnyRoomAccountDataEvent),
+    /// An event for the whole account
+    Global(AnyGlobalAccountDataEvent),
+}
+
+/// Enum allowing to use the same structures for global and room account data
+#[derive(Debug)]
+#[allow(clippy::exhaustive_enums)]
+pub enum AnyRawAccountDataEvent {
+    /// An event for a specific room
+    Room(Raw<AnyRoomAccountDataEvent>),
+    /// An event for the whole account
+    Global(Raw<AnyGlobalAccountDataEvent>),
+}
 
 /// A global account data event.
 #[derive(Clone, Debug, Event)]
