9f4f002d7dc338676f74ef0392c67f81e2568db9
Jason Volk <jason@zemos.net>
2024-10-12 07:38:11 +0000

    add required_keys util to signatures
    Signed-off-by: Jason Volk <jason@zemos.net>

--- crates/ruma-signatures/src/lib.rs
+++ crates/ruma-signatures/src/lib.rs
@@ -67,9 +67,9 @@ pub use self::{
     hash::{content_hash, reference_hash},
     sign::{KeyPair, Signature, hash_and_sign_event, sign_json},
     verify::{
-        PublicKeyMap, PublicKeySet, Verified, required_server_signatures_to_verify_event,
-        to_canonical_json_string_for_signing, verify_canonical_json_bytes, verify_event,
-        verify_json,
+        PublicKeyMap, PublicKeySet, Verified, required_keys,
+        required_server_signatures_to_verify_event, to_canonical_json_string_for_signing,
+        verify_canonical_json_bytes, verify_event, verify_json,
     },
 };
 
