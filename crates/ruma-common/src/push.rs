4a45f63d9f3bedaf1cb0701c4b71dbd6ccf932ba
Jason Volk <jason@zemos.net>
2025-01-23 04:36:37 +0000

    reduce several tracing spans to debug level
    Signed-off-by: Jason Volk <jason@zemos.net>

--- crates/ruma-common/src/push.rs
+++ crates/ruma-common/src/push.rs
@@ -315,7 +315,7 @@ impl Ruleset {
     ///
     /// * `event` - The raw JSON of a room message event.
     /// * `context` - The context of the message and room at the time of the event.
-    #[instrument(skip_all, fields(context.room_id = %context.room_id))]
+    #[instrument(level = "debug", skip_all, fields(context.room_id = %context.room_id))]
     pub async fn get_match<T>(
         &self,
         event: &Raw<T>,
@@ -345,7 +345,7 @@ impl Ruleset {
     ///
     /// * `event` - The raw JSON of a room message event.
     /// * `context` - The context of the message and room at the time of the event.
-    #[instrument(skip_all, fields(context.room_id = %context.room_id))]
+    #[instrument(level = "debug", skip_all, fields(context.room_id = %context.room_id))]
     pub async fn get_actions<T>(
         &self,
         event: &Raw<T>,
