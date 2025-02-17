18fd2cfeed1719fac339a30bf43c9d57743feb45
Aiden McClelland <me@drbonez.dev>
2025-02-17 16:47:13 -0700

    better error reporting

--- crates/ruma-common/src/serde.rs
+++ crates/ruma-common/src/serde.rs
@@ -76,18 +76,24 @@ pub fn is_true(b: &bool) -> bool {
 }
 
 /// Returns None if the serialization fails
-pub fn or_empty<'de, D: Deserializer<'de>, T: Deserialize<'de>>(
+pub fn or_empty<'de, D: Deserializer<'de>, T: for<'a> Deserialize<'a>>(
     deserializer: D,
 ) -> Result<Option<T>, D::Error> {
-    #[derive(serde::Deserialize)]
-    #[serde(untagged)]
-    enum OrEmpty<T> {
-        NotEmpty(T),
-        Empty {},
+    let json = Box::<RawJsonValue>::deserialize(deserializer)?;
+
+    let res = serde_json::from_str::<Option<T>>(json.get()).map_err(de::Error::custom);
+
+    match res {
+        Ok(a) => Ok(a),
+        Err(e) => {
+            #[derive(Deserialize)]
+            #[serde(deny_unknown_fields)]
+            struct Empty {}
+            if let Ok(Empty {}) = serde_json::from_str(json.get()) { Ok(None) } else { Err(e) }
+        }
     }
-    let res = <Option<OrEmpty<T>> as Deserialize<'de>>::deserialize(deserializer)?;
-    Ok(res.and_then(|res| if let OrEmpty::NotEmpty(a) = res { Some(a) } else { None }))
 }
+
 /// Helper function for `serde_json::value::RawValue` deserialization.
 #[inline(never)]
 pub fn from_raw_json_value<'a, T, E>(val: &'a RawJsonValue) -> Result<T, E>
