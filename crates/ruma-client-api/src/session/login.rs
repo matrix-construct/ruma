8ddab2195a98a0eef1bcebaab2e7abefb6c0d195
Jason Volk <jason@zemos.net>
2025-06-18 09:23:45 +0000

    Support org.matrix.login.jwt
    Signed-off-by: Jason Volk <jason@zemos.net>

--- crates/ruma-client-api/src/session/login.rs
+++ crates/ruma-client-api/src/session/login.rs
@@ -151,6 +151,9 @@ pub mod v3 {
         /// Token-based login.
         Token(Token),
 
+        /// JSON Web Token
+        Jwt(Token),
+
         /// Application Service-specific login.
         ApplicationService(ApplicationService),
 
@@ -175,6 +178,9 @@ pub mod v3 {
                     Self::Password(serde_json::from_value(JsonValue::Object(data))?)
                 }
                 "m.login.token" => Self::Token(serde_json::from_value(JsonValue::Object(data))?),
+                "org.matrix.login.jwt" => {
+                    Self::Jwt(serde_json::from_value(JsonValue::Object(data))?)
+                }
                 "m.login.application_service" => {
                     Self::ApplicationService(serde_json::from_value(JsonValue::Object(data))?)
                 }
@@ -219,6 +225,7 @@ pub mod v3 {
             match self {
                 Self::Password(inner) => inner.fmt(f),
                 Self::Token(inner) => inner.fmt(f),
+                Self::Jwt(inner) => inner.fmt(f),
                 Self::ApplicationService(inner) => inner.fmt(f),
                 Self::_Custom(inner) => inner.fmt(f),
             }
@@ -243,6 +250,7 @@ pub mod v3 {
             match login_type {
                 "m.login.password" => from_json_value(data.into()).map(Self::Password),
                 "m.login.token" => from_json_value(data.into()).map(Self::Token),
+                "org.matrix.login.jwt" => from_json_value(data.into()).map(Self::Jwt),
                 "m.login.application_service" => {
                     from_json_value(data.into()).map(Self::ApplicationService)
                 }
