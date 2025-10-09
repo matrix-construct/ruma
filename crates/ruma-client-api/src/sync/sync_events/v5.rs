12e96bc39596d7d0d3ab4cfb399f4304e318ad78
Jason Volk <jason@zemos.net>
2025-10-09 02:52:55 +0000

    client-api: Add `membership` and `lists` to `response::Room`. (MSC4186)
    Signed-off-by: Jason Volk <jason@zemos.net>

--- crates/ruma-client-api/src/sync/sync_events/v5.rs
+++ crates/ruma-client-api/src/sync/sync_events/v5.rs
@@ -635,7 +635,8 @@ pub mod response {
     use ruma_common::profile::UserProfileUpdate;
     use ruma_events::{
         AnyGlobalAccountDataEvent, AnyRoomAccountDataEvent, AnyStrippedStateEvent,
-        AnyToDeviceEvent, receipt::SyncReceiptEvent, typing::SyncTypingEvent,
+        AnyToDeviceEvent, receipt::SyncReceiptEvent, room::member::MembershipState,
+        typing::SyncTypingEvent,
     };
 
     use super::{
@@ -744,6 +745,15 @@ pub mod response {
         /// Heroes of the room.
         #[serde(skip_serializing_if = "Option::is_none")]
         pub heroes: Option<Vec<Hero>>,
+
+        /// The current membership of the user, or omitted if user not in room (for peeking).
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub membership: Option<MembershipState>,
+
+        /// The name of the lists that match this room. The field is omitted if it doesn't match
+        /// any list and is included only due to a subscription.
+        #[serde(default, skip_serializing_if = "Vec::is_empty")]
+        pub lists: Vec<String>,
     }
 
     impl Room {
