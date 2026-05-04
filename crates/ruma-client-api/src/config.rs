6f5940b9a47f0ab30f6174212b1a3b6817539e97
Jason Volk <jason@zemos.net>
2026-05-04 19:36:45 +0000

    client-api: Implement MSC3391 account-data deletion endpoints.
    DELETE /_matrix/client/unstable/org.matrix.msc3391/user/{userId}/
    account_data/{type} and the rooms variant. Gated behind the new
    unstable-msc3391 feature.
    
    Signed-off-by: Jason Volk <jason@zemos.net>

--- crates/ruma-client-api/src/config.rs
+++ crates/ruma-client-api/src/config.rs
@@ -1,5 +1,9 @@
 //! Endpoints for client configuration.
 
+#[cfg(feature = "unstable-msc3391")]
+pub mod delete_global_account_data;
+#[cfg(feature = "unstable-msc3391")]
+pub mod delete_room_account_data;
 pub mod get_global_account_data;
 pub mod get_room_account_data;
 pub mod set_global_account_data;
